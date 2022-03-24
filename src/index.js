9f948fff65afa2abf7aa9f888365ef0931ac08cf
Parangat’s Iphone <swati.s@parangat.com>
2022-03-24 18:40:04 +0530

    Chnaged the version of Web3

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-// import Web3 from 'web3';
-
+import Web3 from 'web3';
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
-import { Web3 } from "web3-react-native";
+// import { Web3 } from "web3-react-native";
 
-const App = () => {
+
+const XDCTestNet = () => {
     // const web3 = new Web3('http://localhost:7545'); // Ganache
     // console.log('Web33333', web3);
     // const newWallet = web3.eth.accounts.wallet.create(1);
     // const newAccount = newWallet[0];
     // console.log('newAAAAAAAAA',newAccount);
+
+    const keystore = 
     useEffect(async () => {
-        const web3 =  await Web3('https://ropsten.infura.io/v3/dc696479f2264b91812f0b0e6faf5326'); // Ganache
-        // web3.Keystore()
-        // const web3 = new Web3('https//rpc.apothem.network/'); 
-        console.log('Web33333',web3);
+        // const password = '123455';
+        // const { Wallet } = await Web3('https://ropsten.infura.io/v3/dc696479f2264b91812f0b0e6faf5326');
+        // console.log('Keystore is',Wallet);
+        // const newwwww = await Wallet.load(keystore, password);
+        // console.log('newwwwwnewwwww is',Wallet,newwwww);
+        const web3 = new Web3('http://localhost:7545'); // Ganache
+        console.log('Web33333', web3);
     }, [])
     return (
         <View>
@@ -22,4 +27,4 @@ const App = () => {
         </View>
     )
 }
-export default App;
+export default XDCTestNet;
