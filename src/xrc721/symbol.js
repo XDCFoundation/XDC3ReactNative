0eed1c7c0b04cec0a0c41e8234c5f3ee017dea88
Angshuman <angshuman@leewayhertz.com>
2022-04-14 15:45:55 +0530

    read method xrc721

---
+++ src/xrc721/symbol.js
@@ -0,0 +1,16 @@
+import { ethers } from 'ethers';
+import xrc721_abi from '../common/xrc721_abi.json';
+// import {url , ERC721 , ERC20} from '../../env'
+
+const _Symbol = (url,token_address) => {
+
+    // Set Provider
+    let httpProvider = new ethers.providers.WebSocketProvider(url)
+    // let contract = new ethers.Contract(ERC721 , xrc721_abi, httpProvider);
+    let contract = new ethers.Contract(token_address , xrc721_abi, httpProvider);
+    let symbol = contract.symbol().then((res) => { return res.toString() })
+            .catch((err) => console.log('error is ',err))
+
+    return symbol;
+}
+export default _Symbol ;
