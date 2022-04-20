6118ae2c0390015198f1f6d7ed8fb420317da095
Parangat’s Iphone <swati.s@parangat.com>
2022-04-20 09:52:10 +0530

    XRC20 Methods

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// import Name from './src/xrc20/name';
-// import Symbol from './src/xrc20/symbol';
-// import Decimal from './src/xrc20/decimals';
-// import BalanceOf from './src/xrc20/balanceOf';
-// import TotalSupply from './src/xrc20/total_supply';
-// import Approve from './src/xrc20/approve';
-// import Allowance from './src/xrc20/allowance';
-// import TransferXdc from './src/xrc20/transfer_xdc';
-// import TransferToken from './src/xrc20/transfer_token';
-// import IncreaseAllowance from './src/xrc20/increase_allowance';
-// import DecreaseAllowance from './src/xrc20/decrease_allowance';
-
 const XDCTestNet = async () => {
    return null;
 }
