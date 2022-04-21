c72b0f78c82aa43d6fb0d413e1b5bbc9df96f80d
Parangat’s Iphone <swati.s@parangat.com>
2022-04-21 11:06:08 +0530

    Write Method ERC20

--- src/xrc20/transfer_from.js
+++ src/xrc20/transfer_from.js
@@ -1,13 +1,13 @@
 import { Contract, ethers, Signer } from 'ethers';
 import xrc20_abi from '../common/xrc20_abi.json';
 
-const transferFromAmount = (url,token_address,privateKey,receiverAddress,spenderAddr,value) => {
+const transferFromAmount = (url,token_address,spendarprivateKey,receiverAddress,spenderAddr,value) => {
     transferfrom = async () => {
         let httpProvider = new ethers.providers.WebSocketProvider(url);
         let gasPrice = await httpProvider.getGasPrice();
         let nonce = await httpProvider.getTransactionCount(spenderAddr);
         let gas_limit = "0x100000"
-        let wallet = new ethers.Wallet(privateKey, httpProvider);
+        let wallet = new ethers.Wallet(spendarprivateKey, httpProvider);
         let contract = new Contract(token_address, xrc20_abi, wallet);
         const amount = BigInt(value);
         let newmethod = await contract.populateTransaction.transferFrom(spenderAddr,receiverAddress, amount);
@@ -18,7 +18,7 @@ const transferFromAmount = (url,token_address,privateKey,receiverAddress,spender
             gasLimit: ethers.utils.hexlify(gas_limit),
             nonce: nonce,
         }
-        let signedTxn = await wallet.signTransaction(txn, privateKey);
+        let signedTxn = await wallet.signTransaction(txn, spendarprivateKey);
         let transfer_from = await httpProvider.sendTransaction(signedTxn);
         return transfer_from;
     }
