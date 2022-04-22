d7ae0c5932a551c50340ea172fd1839b139afa00
Parangat’s Iphone <swati.s@parangat.com>
2022-04-22 14:46:37 +0530

    Final Code

--- src/xrc20/transfer_xdc.js
+++ src/xrc20/transfer_xdc.js
@@ -1,26 +1,27 @@
 import { ethers } from 'ethers';
 
-const TransferXdc = (url,privateKey,owneraddress,receiverAddress,send_token_amount) => {
+const TransferXdc = (url,ownerprivateKey,owneraddress,receiverAddress,send_token_amount) => {
 
     // Set Provider
 
     transferxdc = async () => {
         let httpProvider = new ethers.providers.WebSocketProvider(url);
-        let wallet = new ethers.Wallet(privateKey);
+        let wallet = new ethers.Wallet(ownerprivateKey);
         let walletSigner = wallet.connect(httpProvider);
         let gasPrice = await httpProvider.getGasPrice();
-        let gas_limit = "0x100000"
         let transactionCount = await httpProvider.getTransactionCount(owneraddress);
         const amount = BigInt(send_token_amount);
+        let estimatevalue = await httpProvider.estimateGas({
+            from : owneraddress
+        })
         let txn = {
             to: receiverAddress,
             value: amount,
             gasPrice: gasPrice,
-            gasLimit: ethers.utils.hexlify(gas_limit),
+            gasLimit: estimatevalue,
             nonce: transactionCount,      
         }
-        console.log('Valuee is',txn);
-        let signedTxn = await walletSigner.signTransaction(txn,privateKey);        
+        let signedTxn = await walletSigner.signTransaction(txn,ownerprivateKey);        
         let transfer_xdc = await httpProvider.sendTransaction(signedTxn);
         return transfer_xdc;
     }
